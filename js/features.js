f5a732cd51ad17e2057be817cdbbf635715f0480
sauda_noor <bintinoorsauda@gmail.com>
2024-11-04 00:52:52 +0600

    green button fixed

--- js/features.js
+++ js/features.js
@@ -6,9 +6,17 @@ document.getElementById('btn-show-history').addEventListener('click', function()
 
     //show history form
     document.getElementById('history-form').classList.remove('hidden');
+    
 
     //hide donation form
     document.getElementById('donation-form').classList.add('hidden');
+    
+    
+
+    //green button
+    document.getElementById('btn-show-history').classList.add('bg-[#B4F461]');
+    document.getElementById('btn-show-donation').classList.remove('bg-[#B4F461]');
+
 
 
 })
@@ -23,6 +31,11 @@ document.getElementById('btn-show-donation').addEventListener('click', function(
 
     //hide donation form
     document.getElementById('history-form').classList.add('hidden');
+    document.getElementById('btn-show-history').classList.remove('bg-[#B4F461]');
+
+    //green button
+    document.getElementById('btn-show-donation').classList.add('bg-[#B4F461]');
+    document.getElementById('btn-show-history').classList.remove('bg-[#B4F461]');
 
 
 })
